5c8ca2e20613a063e53f7ae4b427e6a3f6922459
EC2 Default User <ec2-user@ip-172-31-8-64.eu-west-1.compute.internal>
2025-09-04 14:09:46 +0000

    kubebuilder create webhook

--- cmd/main.go
+++ cmd/main.go
@@ -37,6 +37,7 @@ import (
 
 	viewv1 "github.com/wafuwafu13/learn-kubebuilder/api/v1"
 	"github.com/wafuwafu13/learn-kubebuilder/internal/controller"
+	webhookv1 "github.com/wafuwafu13/learn-kubebuilder/internal/webhook/v1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -184,6 +185,13 @@ func main() {
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "MarkdownView")
 		os.Exit(1)
+	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := webhookv1.SetupMarkdownViewWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "MarkdownView")
+			os.Exit(1)
+		}
 	}
 	// +kubebuilder:scaffold:builder
 
